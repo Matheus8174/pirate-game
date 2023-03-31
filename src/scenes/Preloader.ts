4a600052db2d086363e9ced0c37c9b9fb6813f51
Matheus Araújo <souzamatheus8174@gmail.com>
2023-03-31 18:22:51 -0300

    add more assets to load

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -5,8 +5,22 @@ class Preloader extends Scene {
     super('preloader');
   }
 
+  private loadPalmTree() {
+    this.load.atlas('front-palm-tree-top', 'front-palm-tree-top.png', 'front-palm-tree-top.json');
+    this.load.atlas('back-palm-tree', 'back-palm-tree.png', 'back-palm-tree.json');
+  }
+
+  private loadClouds() {
+    this.load.image('small-cloud', 'Treasure Hunters/Treasure Hunters/Palm Tree Island/Sprites/Background/Small Cloud 2.png');
+    this.load.image('small-cloud-02', 'Treasure Hunters/Treasure Hunters/Palm Tree Island/Sprites/Background/Small Cloud 3.png');
+    this.load.image('big-cloud', 'Treasure Hunters/Treasure Hunters/Palm Tree Island/Sprites/Background/Big Clouds.png');
+  }
+
   private loadTiles() {
-    this.load.image('tiles', 'Treasure Hunters/Treasure Hunters/Palm Tree Island/Sprites/Terrain/Terrain (32x32).png');
+    this.load.image('grass-and-palm-trees', 'Treasure Hunters/Treasure Hunters/Palm Tree Island/Sprites/Front Palm Trees/Front Palm Bottom and Grass (32x32).png');
+    this.load.image('terrain', 'Treasure Hunters/Treasure Hunters/Palm Tree Island/Sprites/Terrain/Terrain (32x32).png');
+    this.load.image('background', 'Treasure Hunters/Treasure Hunters/Palm Tree Island/Sprites/Background/BG Image.png');
+
     this.load.tilemapTiledJSON('platform', 'game.json');
   }
 
@@ -14,13 +28,20 @@ class Preloader extends Scene {
     this.load.atlas('captain', 'captain.png', 'captain.json');
   }
 
+  private loadWaterEffects() {
+    this.load.atlas('water-reflect', 'water-reflect.png', 'water-reflect.json');
+  }
+
   public preload() {
     this.loadTiles();
     this.loadCharacters();
+    this.loadWaterEffects();
+    this.loadClouds();
+    this.loadPalmTree();
   }
 
   public create() {
-    this.scene.start('game');
+    this.scene.start('create-map');
   }
 }
 
